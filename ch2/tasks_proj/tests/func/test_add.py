5af0595c1eb531cf4d9eaed5dece4d3324fa49e0
Ankit Goel <ankitgoel616@gmail.com>
2018-09-25 21:00:40 +0530

    Add smoke tests using fixtures

--- ch2/tasks_proj/tests/func/test_add.py
+++ ch2/tasks_proj/tests/func/test_add.py
@@ -1,10 +1,28 @@
-"""
-Placeholder test file.
+import pytest
+import tasks
+from tasks import Task
 
-We'll add a bunch of tests here in later versions.
-"""
+@pytest.fixture(autouse=True)
+def initialized_tasks_db(tmpdir):
+    """Connect to the db before testing, disconnect after."""
+    tasks.start_tasks_db(str(tmpdir), 'tiny')
 
+    yield # This is where the testing happens
 
-def test_add():
-    """Placeholder test."""
-    pass
+    tasks.stop_tasks_db()
+
+def test_add_returns_valid_id():
+    """tasks.add(<valid task>) should return an integer."""
+    new_task = Task('do something')
+    task_id = tasks.add(new_task)
+    assert isinstance(task_id, int)
+
+@pytest.mark.smoke
+def test_added_task_has_id_set():
+    """task_id field is set by tasks.add()."""
+    new_task = Task('sit in chair', owner='me', done=True)
+    task_id = tasks.add(new_task)
+
+    task_from_db = tasks.get(task_id)
+
+    assert task_from_db.id == task_id
