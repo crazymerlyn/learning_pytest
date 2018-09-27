ec1b08049bcc3459fba0759ca117be50cd6a2872
Ankit Goel <ankitgoel616@gmail.com>
2018-09-27 22:35:34 +0530

    Add fixtures which prepare the database

--- ch3/tasks_proj/tests/func/test_add.py
+++ ch3/tasks_proj/tests/func/test_add.py
@@ -7,3 +7,8 @@ def test_add_return_valid_id(tasks_db):
     new_task = Task('do something')
     task_id = tasks.add(new_task)
     assert isinstance(task_id, int)
+
+def test_add_increases_count(db_with_3_tasks):
+    """tasks.add() increases tasks.count() by 1."""
+    tasks.add(Task('throw a party'))
+    assert tasks.count() == 4
