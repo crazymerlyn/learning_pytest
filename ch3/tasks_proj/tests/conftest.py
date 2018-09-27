c5e88fac72688a10691a93f846fc74b8da853b1b
Ankit Goel <ankitgoel616@gmail.com>
2018-09-27 20:07:47 +0530

    Create a common fixture in conftest.py

--- ch3/tasks_proj/tests/conftest.py
+++ ch3/tasks_proj/tests/conftest.py
@@ -1,3 +1,10 @@
-"""Placeholder."""
+import pytest
+import tasks
+from tasks import Task
 
-# nothing here yet
+@pytest.fixture()
+def tasks_db(tmpdir):
+    """Connect to db before tests, disconnect after."""
+    tasks.start_tasks_db(str(tmpdir), 'tiny')
+    yield
+    tasks.stop_tasks_db()
