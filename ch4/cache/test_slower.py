2b447add940f9ea00740c72266a53b607f227d03
Ankit Goel <ankitgoel616@gmail.com>
2018-09-28 23:48:28 +0530

    Only update cache once a session

--- ch4/cache/test_slower.py
+++ ch4/cache/test_slower.py
@@ -2,17 +2,28 @@ import datetime
 import pytest
 import random
 import time
+from collections import namedtuple
+
+Duration = namedtuple('Duration', ['current', 'last'])
+
+@pytest.fixture(scope='session')
+def duration_cache(request):
+    key = 'duration/testdurations'
+    d = Duration({}, request.config.cache.get(key, {}))
+    yield d
+    request.config.cache.set(key, d.current)
 
 @pytest.fixture(autouse=True)
-def check_duration(request, cache):
-    key = 'duration/' + request.node.nodeid.replace(':', '_')
+def check_duration(request, duration_cache):
+    d = duration_cache
+    nodeid = request.node.nodeid
     start_time = datetime.datetime.now()
     yield
     stop_time = datetime.datetime.now()
 
     this_duration = (stop_time - start_time).total_seconds()
-    last_duration = cache.get(key, None)
-    cache.set(key, this_duration)
+    d.current[nodeid] = this_duration
+    last_duration = d.last.get(nodeid, None)
     if last_duration is not None:
         errorstring = "test duration over 2x last duration"
         assert this_duration <= last_duration * 2, errorstring
