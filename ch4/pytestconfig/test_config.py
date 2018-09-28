e5fb4acf00a3a3547c941225a64720ae7f14c06e
Ankit Goel <ankitgoel616@gmail.com>
2018-09-28 19:19:18 +0530

    Create fixtures for specific options

--- ch4/pytestconfig/test_config.py
+++ ch4/pytestconfig/test_config.py
@@ -1,5 +1,13 @@
 import pytest
 
-def test_option(pytestconfig):
-    print('"foo" set to:', pytestconfig.getoption('foo'))
-    print('"myopt" set to:', pytestconfig.getoption('myopt'))
+@pytest.fixture()
+def foo(pytestconfig):
+    return pytestconfig.option.foo
+
+@pytest.fixture()
+def myopt(pytestconfig):
+    return pytestconfig.option.myopt
+
+def test_option(foo, myopt):
+    print('"foo" set to:', foo)
+    print('"myopt" set to:', myopt)
