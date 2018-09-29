18baa2d456decdd7fb2383b8431d331a4562367b
Ankit Goel <ankitgoel616@gmail.com>
2018-09-29 18:53:58 +0530

    Change expanduser instead of HOME for portability

--- ch4/monkey/test_cheese.py
+++ ch4/monkey/test_cheese.py
@@ -1,7 +1,9 @@
 import cheese
 
-def test_def_pref_full(tmpdir, monkeypatch):
-    monkeypatch.setenv('HOME', tmpdir.mkdir('home'))
+def test_def_prefs_change_expanduser(tmpdir, monkeypatch):
+    fake_home_dir = tmpdir.mkdir('home')
+    monkeypatch.setattr(cheese.os.path, 'expanduser',
+                        (lambda x: x.replace('~', str(fake_home_dir))))
     cheese.write_default_cheese_preferences()
     expected = cheese._default_prefs
     actual = cheese.read_cheese_preferences()
