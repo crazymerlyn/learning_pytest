4bfbb06d5c0746687965f9ae04ff1f520b53e7c7
Ankit Goel <ankitgoel616@gmail.com>
2018-09-29 18:50:23 +0530

    Use monkeypatch to change HOME environment variable for a test

--- ch4/monkey/test_cheese.py
+++ ch4/monkey/test_cheese.py
@@ -1,6 +1,7 @@
 import cheese
 
-def test_def_pref_full():
+def test_def_pref_full(tmpdir, monkeypatch):
+    monkeypatch.setenv('HOME', tmpdir.mkdir('home'))
     cheese.write_default_cheese_preferences()
     expected = cheese._default_prefs
     actual = cheese.read_cheese_preferences()
