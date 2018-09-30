08bb1b90e9f615a83fff15c7fac7340aa89a7aba
Ankit Goel <ankitgoel616@gmail.com>
2018-09-30 10:59:45 +0530

    Use recwarn to record and test warnings

---
+++ ch4/test_warnings.py
@@ -0,0 +1,13 @@
+import warnings
+import pytest
+
+def lame_function():
+    warnings.warn('Please stop using this', DeprecationWarning)
+    # rest of function
+
+def test_lame_function(recwarn):
+    lame_function()
+    assert len(recwarn) == 1
+    w = recwarn.pop()
+    assert w.category == DeprecationWarning
+    assert str(w.message) == 'Please stop using this'
