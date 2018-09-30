eedc86727c8553c022de4ddece914c7a6214d8c8
Ankit Goel <ankitgoel616@gmail.com>
2018-09-30 10:44:01 +0530

    Make sure cheese config overwrites existing file

--- ch4/monkey/test_cheese.py
+++ ch4/monkey/test_cheese.py
@@ -1,10 +1,20 @@
 import cheese
+import copy
 
-def test_def_prefs_change_expanduser(tmpdir, monkeypatch):
+def test_def_prefs_change_defaults(tmpdir, monkeypatch):
     fake_home_dir = tmpdir.mkdir('home')
     monkeypatch.setattr(cheese.os.path, 'expanduser',
                         (lambda x: x.replace('~', str(fake_home_dir))))
     cheese.write_default_cheese_preferences()
-    expected = cheese._default_prefs
+    defaults_before = copy.deepcopy(cheese._default_prefs)
+
+    monkeypatch.setitem(cheese._default_prefs, 'slicing', ['provolone'])
+    monkeypatch.setitem(cheese._default_prefs, 'spreadable', ['brie'])
+    monkeypatch.setitem(cheese._default_prefs, 'salads', ['pepper jack'])
+    defaults_modified = cheese._default_prefs
+
+    cheese.write_default_cheese_preferences()
+
     actual = cheese.read_cheese_preferences()
-    assert expected == actual
+    assert defaults_modified == actual
+    assert defaults_modified != defaults_before
