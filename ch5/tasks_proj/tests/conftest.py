bbc6d690a655e577e329fbbbb823a3b4d06ead1b
Ankit Goel <ankitgoel616@gmail.com>
2018-09-30 12:17:42 +0530

    Guard the nice functionality with a --nice option

--- ch5/tasks_proj/tests/conftest.py
+++ ch5/tasks_proj/tests/conftest.py
@@ -2,13 +2,20 @@ import pytest
 import tasks
 from tasks import Task
 
+def pytest_addoption(parser):
+    """Turn nice features on with --nice option."""
+    group = parser.getgroup('nice')
+    group.addoption('--nice', action='store_true',
+                    help='nice: turn failures into opportunities')
+
 def pytest_report_header():
     """Thank tester for running tests."""
-    return "Thanks for running the tests."""
+    if pytest.config.getoption('nice'):
+        return "Thanks for running the tests."""
 
 def pytest_report_teststatus(report):
     """Turn failures into opportunities."""
-    if report.when == 'call' and report.failed:
+    if report.when == 'call' and pytest.config.getoption('nice') and report.failed:
         return (report.outcome, 'O', 'OPPORTUNITY for improvement')
 
 @pytest.fixture(scope='session')
